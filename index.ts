9c10650a84ff0acbd830bd0ac0f6e7a17738cce8
Skyclo <hello.skyclo@gmail.com>
2021-10-28 17:20:06 -0400

    🚧 wip: add packages

--- index.ts
+++ index.ts
@@ -2,13 +2,26 @@
 /*                                             MODULES                                            */
 /* ---------------------------------------------------------------------------------------------- */
 /* ----------------------------------------- HTTP Server ---------------------------------------- */
-import express from 'express'
+// This module is responsible for creating the HTTP server.
+import express          from 'express'
+// Compresses each request/response
+import compression      from 'compression'
+// Parses the request body
+import bodyParser       from 'body-parser'
+// Parses the request cookies
+import cookieParser     from 'cookie-parser'
+// Implements certain security features and hardens the express server
+import helmet           from 'helmet'
+/* ------------------------------------------ Utilities ----------------------------------------- */
+// Pretty colors or something idk
+import pc               from 'picocolors'
 
 
 /* ---------------------------------------------------------------------------------------------- */
 /*                                              INIT                                              */
 /* ---------------------------------------------------------------------------------------------- */
 /* ----------------------------------------- HTTP Server ---------------------------------------- */
+// This creates the express server
 const app = express()
 
 
